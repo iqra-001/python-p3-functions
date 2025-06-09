a3a1fd67764d0006e5212044241b7ef4da005998
iqra <abdikarimiqra@gmail.com>
2025-06-09 15:04:18 +0300

    assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
 
-def greet(name):
-    pass
+greet_programmer()
+
+
+def greet(name = "Naureen"):
+   print(f"Hello, {name}!")
+
+
+greet()
+
+ 
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
+
+
+print(greet_with_default("iqra"))
+
+   
 
 def add(num1, num2):
-    pass
+    result = num1 + num2
+    return result
+
+add(1,2)
+
 
 def halve(number):
-    pass
+    return number/2
+
+halve(4)
